6937324dcca4ea5341cf4afc6caef946cff49e68
Demi god <moneshram4@gmail.com>
2025-10-03 18:07:22 +0530

    Pandas Library

---
+++ Additional Lib PY/01_Pandas.py
@@ -0,0 +1,20 @@
+# Pandas
+'''
+pandas it is refers to a software library written for the Python programming language,
+primarily for data manipulation and analysis. It provides data structures like DataFrame and Series,
+which are optimized for handling tabular data, and tools for reading and writing data from various file formats.
+'''
+
+import pandas as pd
+
+data = {
+    'cars' : ['bmw','audi','mercedes'],
+    'price' : [200,300,400]
+}
+
+x = pd.DataFrame(data)
+print(type(x))
+print(x)
+print(x['cars'])
+print(x['price'])
+print(pd.__version__)
