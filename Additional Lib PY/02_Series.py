45225ccb20a99c70f6c9878005ba54e4b3e4cb28
Demi god <moneshram4@gmail.com>
2025-10-03 19:31:35 +0530

    Series function in pandas

---
+++ Additional Lib PY/02_Series.py
@@ -0,0 +1,26 @@
+# Series
+'''
+Series is a one-dimensional labeled array capable of holding any data type (integers, strings, floating-point numbers, Python objects, etc.).
+It is similar to a column in a spreadsheet or a database table.
+'''
+
+import pandas as pd
+
+a = [1,3,5,7,9]
+
+y = pd.Series(a,index=['a','b','c','d','e']) # capital S used for Series
+print(y)
+# print(y[1])
+
+
+b = [1,'mrm',2,3]
+z = pd.Series(b,index=['a','1','c','d'])
+print(z)
+print(z['c'])
+
+
+
+calories = {'day1': 420, 'day2': 380, 'day3': 390}
+x = pd.Series(calories)
+x = pd.Series(calories, index=['day1'])
+print(x)
