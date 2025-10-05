66676348091389519b866236b62edfcee84b60b4
Demi god <moneshram4@gmail.com>
2025-10-05 18:06:42 +0530

    Modified

--- Additional Lib PY/04(a)_Read CSV Files.py
+++ Additional Lib PY/04(a)_Read CSV Files.py
@@ -8,3 +8,12 @@ import pandas as pd
 df = pd.read_csv('Additional Lib PY\(a)_data.csv')
 print(df)
 print(df.to_string())
+
+#  Part 2 
+
+print(df.info()) # return the information about the dataframe
+
+print(df.head()) # return the first 5 rows
+print(df.head(10)) # return the first 10 rows
+print(df.tail()) # return the last 5 rows
+print(df.tail(10)) # return the last 10 rows
